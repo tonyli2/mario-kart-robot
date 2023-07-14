135fc3479393e8aee8f62ec186564261e8525c25
Hunter Ma <hunterma@dhcp-206-12-137-197.ubcsecure.wireless.ubc.ca>
2023-07-14 12:11:03 -0700

    Fixed display issue

--- src/main.cpp
+++ src/main.cpp
@@ -78,101 +78,104 @@ String processOutput(float_t output) {
 
 void setup()
 {   
-    //setup servo
-    // DigitalPID::setupServo();
-    servo.attach(STEERING_SERVO);
+  //setup servo
+  //DigitalPID::setupServo();
+  servo.attach(STEERING_SERVO);
 
-    //setup Oled
-    //OledDisplay::setupDisplay();
-    display_handler.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-    display_handler.display();
+  //setup Oled
+  //OledDisplay::setupDisplay();
+  display_handler.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display_handler.display();
+  display_handler.setTextSize(1);
+  display_handler.setTextColor(SSD1306_WHITE);
 
-    delay(1000);
+  delay(1000);
 }
 
 void loop()
 {
   display_handler.clearDisplay();
+  display_handler.setCursor(0, 0);
   display_handler.println(DigitalPID::applyPID());
   display_handler.println(counter++);
 
-    // for(int i = 45; i < 130; i++) {
-    //     display_handler.clearDisplay();
-    //     display_handler.setTextSize(1);
-    //     display_handler.setTextColor(SSD1306_WHITE);
-    //     display_handler.setCursor(0, 0);
-    //     display_handler.println("Servo angle ");
-    //     display_handler.println(i);
-    //     servo.write(i);
-    //     display_handler.display();
-    //     delay(100);
-    // }
+  // for(int i = 45; i < 130; i++) {
+  //     display_handler.clearDisplay();
+  //     display_handler.setTextSize(1);
+  //     display_handler.setTextColor(SSD1306_WHITE);
+  //     display_handler.setCursor(0, 0);
+  //     display_handler.println("Servo angle ");
+  //     display_handler.println(i);
+  //     servo.write(i);
+  //     display_handler.display();
+  //     delay(100);
+  // }
 
-    //Max is 35
+  //Max is 35
 
-    // int i = 90;
-    // display_handler.clearDisplay();
-    // display_handler.setTextSize(1);
-    // display_handler.setTextColor(SSD1306_WHITE);
-    // display_handler.setCursor(0, 0);
-    // display_handler.println("Servo angle ");
-    // display_handler.println(i);
-    // servo.write(i);
-    // display_handler.display();
-    // delay(1000);
+  // int i = 90;
+  // display_handler.clearDisplay();
+  // display_handler.setTextSize(1);
+  // display_handler.setTextColor(SSD1306_WHITE);
+  // display_handler.setCursor(0, 0);
+  // display_handler.println("Servo angle ");
+  // display_handler.println(i);
+  // servo.write(i);
+  // display_handler.display();
+  // delay(1000);
 
-    // float_t left = analogRead(LEFT_TAPE_PIN);
-    // float_t right = analogRead(RIGHT_TAPE_PIN);
+  // float_t left = analogRead(LEFT_TAPE_PIN);
+  // float_t right = analogRead(RIGHT_TAPE_PIN);
 
-    // display_handler.clearDisplay();
-    // display_handler.setTextSize(1);
-    // display_handler.setTextColor(SSD1306_WHITE);
-    // display_handler.setCursor(0, 0);
-    // display_handler.print("Left: ");
-    // display_handler.println(left);
-    // display_handler.print("Right: ");
-    // display_handler.println(right);
+  // display_handler.clearDisplay();
+  // display_handler.setTextSize(1);
+  // display_handler.setTextColor(SSD1306_WHITE);
+  // display_handler.setCursor(0, 0);
+  // display_handler.print("Left: ");
+  // display_handler.println(left);
+  // display_handler.print("Right: ");
+  // display_handler.println(right);
 
-    // int16_t error = func(left,right);
+  // int16_t error = func(left,right);
 
-    // float_t derivative = (error - previousError) / 100;
+  // float_t derivative = (error - previousError) / 100;
 
-    // float_t output = 50.0f * error + 5.0f * derivative;
-    // display_handler.print("Error:");
-    // display_handler.println(error);
-    // display_handler.print("Output:");
-    // display_handler.println(output);
-    // display_handler.println(processOutput(output));
-    // display_handler.println(counter++);
+  // float_t output = 50.0f * error + 5.0f * derivative;
+  // display_handler.print("Error:");
+  // display_handler.println(error);
+  // display_handler.print("Output:");
+  // display_handler.println(output);
+  // display_handler.println(processOutput(output));
+  // display_handler.println(counter++);
   int8_t duty_cycle = 50;
   DriverMotors::startMotorsForward(duty_cycle);
   String duty_cycle_print = "Duty Cycle: " + String(duty_cycle);
   display_handler.println(duty_cycle_print);
 
-    // if(left > L_THRESHOLD && right < R_THRESHOLD){
-    //   //Left is off tape and Right is on tape
-    //     display_handler.println("TURN RIGHT");
+  // if(left > L_THRESHOLD && right < R_THRESHOLD){
+  //   //Left is off tape and Right is on tape
+  //     display_handler.println("TURN RIGHT");
 
-    // }
-    // else if(left < L_THRESHOLD && right > R_THRESHOLD){
-    //   //left is on tape and Right is off tape
-    //     display_handler.println("TURN LEFT");
+  // }
+  // else if(left < L_THRESHOLD && right > R_THRESHOLD){
+  //   //left is on tape and Right is off tape
+  //     display_handler.println("TURN LEFT");
 
-    // }
-    // // else if(left > L_THRESHOLD && right > R_THRESHOLD){
-    // //   //Left is off tape and Right is off tape
-    // //   // TODO Look at previous error state and magnify it
-    // //   return previousError * 2;
-    // // }
-    // else{
-    //     display_handler.println("GO STRAIGHT");
-    // }
+  // }
+  // // else if(left > L_THRESHOLD && right > R_THRESHOLD){
+  // //   //Left is off tape and Right is off tape
+  // //   // TODO Look at previous error state and magnify it
+  // //   return previousError * 2;
+  // // }
+  // else{
+  //     display_handler.println("GO STRAIGHT");
+  // }
 
-    if(counter > 1000){
-        counter = 0;
-    }
+  if(counter > 1000){
+      counter = 0;
+  }
 
-    // previousError = error;
-    display_handler.display();
-    
+  // previousError = error;
+  display_handler.display();
+  
 }
