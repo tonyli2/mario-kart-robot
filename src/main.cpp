5ffdb881d19be7db83cc098176432c1d256ff665
Hunter Ma <hunterma@dhcp-206-12-137-197.ubcsecure.wireless.ubc.ca>
2023-07-14 12:28:39 -0700

    Fixed servo issue

--- src/main.cpp
+++ src/main.cpp
@@ -17,70 +17,70 @@ Adafruit_SSD1306 display_handler(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET)
 uint32_t counter = 0;
 float_t L_THRESHOLD = 600;
 float_t R_THRESHOLD = 600;
-Servo servo;
+// Servo servo;
 int16_t previousError = 0;
 
-int16_t func(float_t left, float_t right);
+// int16_t func(float_t left, float_t right);
 
-int16_t func(float_t left, float_t right){
-    /*
-      -1: Left is off tape
-      0: both on tape
-      1: right is on tape
-      2: both off tape
-    */
+// int16_t func(float_t left, float_t right){
+//     /*
+//       -1: Left is off tape
+//       0: both on tape
+//       1: right is on tape
+//       2: both off tape
+//     */
 
-    if(left > L_THRESHOLD && right < R_THRESHOLD){
-      //Left is off tape and Right is on tape
-      //TURN RIGHT
-      return -1;
-    }
-    else if(left < L_THRESHOLD && right > R_THRESHOLD){
-      //left is on tape and Right is off tape
-      //TURN LEFT
-      return 1;
-    }
-    else if(left > L_THRESHOLD && right > R_THRESHOLD){
-      //Left is off tape and Right is off tape
-      // TODO Look at previous error state and magnify it
-      return previousError * 2;
-    }
-    else{
-      return 0;
-    }
-}
+//     if(left > L_THRESHOLD && right < R_THRESHOLD){
+//       //Left is off tape and Right is on tape
+//       //TURN RIGHT
+//       return -1;
+//     }
+//     else if(left < L_THRESHOLD && right > R_THRESHOLD){
+//       //left is on tape and Right is off tape
+//       //TURN LEFT
+//       return 1;
+//     }
+//     else if(left > L_THRESHOLD && right > R_THRESHOLD){
+//       //Left is off tape and Right is off tape
+//       // TODO Look at previous error state and magnify it
+//       return previousError * 2;
+//     }
+//     else{
+//       return 0;
+//     }
+// }
 
-String processOutput(float_t output) {
+// String processOutput(float_t output) {
 
-    //Limits output angle
-    if(output > 50.0f){
-      output = 50.0f;
-    }
-    else if(output < -50.0f){
-      output = -50.0f;
-    }
+//     //Limits output angle
+//     if(output > 50.0f){
+//       output = 50.0f;
+//     }
+//     else if(output < -50.0f){
+//       output = -50.0f;
+//     }
 
-    //Process servo angle from PID output
-    if(output < 0.0f){
-      servo.write(90 - output);
-      return "OFunc: Turn right";
-    }
-    else if(output > 0.0f){
-      servo.write(90 - output);
-      return "OFunc: Turn left";
-    }
-    else {
-      servo.write(90 + output);
-      return "OFunc: Go straight";
-    }
-  }
+//     //Process servo angle from PID output
+//     if(output < 0.0f){
+//       servo.write(90 - output);
+//       return "OFunc: Turn right";
+//     }
+//     else if(output > 0.0f){
+//       servo.write(90 - output);
+//       return "OFunc: Turn left";
+//     }
+//     else {
+//       servo.write(90 + output);
+//       return "OFunc: Go straight";
+//     }
+//   }
 
 
 void setup()
 {   
   //setup servo
-  //DigitalPID::setupServo();
-  servo.attach(STEERING_SERVO);
+  DigitalPID::setupServo();
+  // servo.attach(STEERING_SERVO);
 
   //setup Oled
   //OledDisplay::setupDisplay();
@@ -147,7 +147,7 @@ void loop()
   // display_handler.println(output);
   // display_handler.println(processOutput(output));
   // display_handler.println(counter++);
-  int8_t duty_cycle = 50;
+  int8_t duty_cycle = 30;
   DriverMotors::startMotorsForward(duty_cycle);
   String duty_cycle_print = "Duty Cycle: " + String(duty_cycle);
   display_handler.println(duty_cycle_print);
