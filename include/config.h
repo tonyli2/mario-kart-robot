eac28b6e2594519e5e49c44c2cdffd7cfc115000
Tony Li <dawner125@gmail.com>
2023-07-18 20:36:08 -0700

    Setup Serial Monitor

--- include/config.h
+++ include/config.h
@@ -8,18 +8,22 @@
 #define SCREEN_HEIGHT                   64 // OLED display height, in pixels
 #define OLED_RESET                      -1    // This display does not have a reset pin accessible
 
-// Tape Reflectance Sensor L R
+// Tape Reflectance Sensor L R (Analog)
 #define LEFT_TAPE_PIN                   PA0
 #define RIGHT_TAPE_PIN                  PA1
 
+// Marker Reflectance Sensors L R (Digital)
+#define MARKER_SENSE_LEFT               PB0
+#define MARKER_SENSE_RIGHT              PB1
+#define INTERRUPT_PIN                   PB7
 
+// IR sensors (Analog)
+#define IR_DETECTOR_LEFT                PA4
+#define IR_DETECTOR_RIGHT               PA5
 
-// IR sensors
-#define IR_DETECTOR_LEFT                PA3
-
-// Steering servo PWM pin
+// Steering servo pin (PWM)
 #define STEERING_SERVO                  PA_10
 
-// Motor driving PWM pins
-#define DRIVING_PIN_FORWARD             PA_8
-#define DRIVING_PIN_BACKWARD            PA_9
+// Motor driving pins (PWM)
+#define DRIVING_PIN_FORWARD             PB_8
+#define DRIVING_PIN_BACKWARD            PB_9
--- src/main.cpp
+++ src/main.cpp
@@ -22,11 +22,15 @@ void setup() {
   // Set up servo
   DigitalPID::setupServo();
 
+  //Setup Serial Monitor
+  Serial.begin(9600);
+
+
   // Set up OLED
-  display_handler.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display_handler.display();
-  display_handler.setTextSize(1);
-  display_handler.setTextColor(SSD1306_WHITE);
+  // display_handler.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  // display_handler.display();
+  // display_handler.setTextSize(1);
+  // display_handler.setTextColor(SSD1306_WHITE);
 
   int8_t IMUReady = SensorFusion::IMUInit();
   if (IMUReady > 0) {
@@ -39,90 +43,15 @@ void setup() {
 }
 
 void loop() {
-  display_handler.clearDisplay();
-  display_handler.setCursor(0, 0);
-  display_handler.println(DigitalPID::applySteeringPID());
-  display_handler.println(counter++);
 
-  float_t *attitude_vec = SensorFusion::IMUGetData();
-  printIMU(attitude_vec);
-
-  // for(int i = 45; i < 130; i++) {
-  //     display_handler.clearDisplay();
-  //     display_handler.setTextSize(1);
-  //     display_handler.setTextColor(SSD1306_WHITE);
-  //     display_handler.setCursor(0, 0);
-  //     display_handler.println("Servo angle ");
-  //     display_handler.println(i);
-  //     servo.write(i);
-  //     display_handler.display();
-  //     delay(100);
-  // }
-
-  //Max is 35
-
-  // int i = 90;
-  // display_handler.clearDisplay();
-  // display_handler.setTextSize(1);
-  // display_handler.setTextColor(SSD1306_WHITE);
-  // display_handler.setCursor(0, 0);
-  // display_handler.println("Servo angle ");
-  // display_handler.println(i);
-  // servo.write(i);
-  // display_handler.display();
-  // delay(1000);
-
-  // float_t left = analogRead(LEFT_TAPE_PIN);
-  // float_t right = analogRead(RIGHT_TAPE_PIN);
-
-  // display_handler.clearDisplay();
-  // display_handler.setTextSize(1);
-  // display_handler.setTextColor(SSD1306_WHITE);
-  // display_handler.setCursor(0, 0);
-  // display_handler.print("Left: ");
-  // display_handler.println(left);
-  // display_handler.print("Right: ");
-  // display_handler.println(right);
-
-  // int16_t error = func(left,right);
-
-  // float_t derivative = (error - previousError) / 100;
-
-  // float_t output = 50.0f * error + 5.0f * derivative;
-  // display_handler.print("Error:");
-  // display_handler.println(error);
-  // display_handler.print("Output:");
-  // display_handler.println(output);
-  // display_handler.println(processOutput(output));
-  // display_handler.println(counter++);
-  
-
-  // if(left > L_THRESHOLD && right < R_THRESHOLD){
-  //   //Left is off tape and Right is on tape
-  //     display_handler.println("TURN RIGHT");
-
-  // }
-  // else if(left < L_THRESHOLD && right > R_THRESHOLD){
-  //   //left is on tape and Right is off tape
-  //     display_handler.println("TURN LEFT");
-
-  // }
-  // // else if(left > L_THRESHOLD && right > R_THRESHOLD){
-  // //   //Left is off tape and Right is off tape
-  // //   // TODO Look at previous error state and magnify it
-  // //   return previousError * 2;
-  // // }
-  // else{
-  //     display_handler.println("GO STRAIGHT");
-  // }
+  Serial.println(counter++);
 
   if(counter > 1000) {
       counter = 0;
   }
 
-  display_handler.display();
   
-  delay(50);
+  delay(1000);
 }
 
 void printIMU(float_t *attitude_vec) {
