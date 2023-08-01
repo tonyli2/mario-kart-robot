86437bf483b7300341361839e9783e762891dac8
Tony Li <dawner125@gmail.com>
2023-08-01 12:09:38 -0700

    Moved Servo PWM pin to A10

--- include/config.h
+++ include/config.h
@@ -36,7 +36,7 @@
 #define IR_DETECTOR_RIGHT               PA5
 
 // Steering servo pin (PWM)
-#define STEERING_SERVO                  PA_8
+#define STEERING_SERVO                  PA_10
 
 // Motor driving pins (PWM)
 #define LEFT_DRIVING_FWD                PB_9
