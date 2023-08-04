119f5f498284a6b78b8c5386318e8e94c97b03b3
Tony Li <dawner125@gmail.com>
2023-08-03 19:46:35 -0700

    included analog pid files

---
+++ include/analog_pid.h
@@ -0,0 +1,21 @@
+// #ifndef SERVO.H
+// #define SERVO.H
+
+// #include <Servo.h>
+
+// #endif // Guard for servo.h
+
+// namespace AnalogPID
+// {
+
+//     const float_t fjkldasfj = 4.0;
+
+//     void applyPID(Servo servo);
+
+//     // void tester(float_t f1, float_t f2);
+
+//     void calcError(float *left, float *right);
+
+//     // void processOutput(float_t *output, bool applyDifferential, Servo servo);
+
+// } // namespace AnalogPID
---
+++ src/analog_pid.cpp
@@ -0,0 +1,16 @@
+// #include <config.h>
+
+// namespace AnalogPID {
+
+//     // void applyPID(Servo servo){
+
+//     // }
+
+//     // void calcError(float_t *left, float_t *right){
+
+//     // }
+
+//     // void processOutput(float_t *output, bool applyDifferential, Servo servo) {
+
+//     // }
+// }
